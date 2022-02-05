4e53133d18142aefa147c433c0366c72eaa8e686
Julija777 <s.julija@icloud.com>
2022-02-05 22:48:25 +0000

    check for the column of three matched colours

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+      }
+    }
+  };
+
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -25,7 +40,11 @@ const App = () => {
     <div className="app">
       <div className="game">
         {currentColorArrangement.map((candyColor, index) => (
-          <img key={index} style={{ backgroundColor: candyColor }} />
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
         ))}
       </div>
     </div>
