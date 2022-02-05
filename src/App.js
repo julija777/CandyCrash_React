70f02ae3dc4b445c9ff66152d35b986beabb722b
Julija777 <s.julija@icloud.com>
2022-02-05 20:57:11 +0000

    Board with 64 rundom cells created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
+
 const width = 8;
 const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -9,10 +12,24 @@ const App = () => {
         candyColors[Math.floor(Math.random() * candyColors.length)];
       randomColorArrangement.push(randomColor);
     }
+    setCurrentColorArrangement(randomColorArrangement);
   };
 
-  createBoard();
-  return <div>Candy Crash</div>;
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  console.log(currentColorArrangement);
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img key={index} style={{ backgroundColor: candyColor }} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
